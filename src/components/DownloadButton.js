0b395b8879372a43ab96cab6260c2aaac4b3ba58
Jonathan Hemnes <jhemnes+github@gmail.com>
2022-06-28 12:06:02 -0600

    fix: link

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -3,7 +3,7 @@ import {Button, Box} from "theme-ui"
 
 export function DownloadButton() {
 	function handleDownload() {
-		window.location = "/vcard.vcf"
+		window.location = "/vcard/vcard.vcf"
 	}
 	return (
 		<Box sx={{position: 'fixed', right: '50px', bottom: '40px', zIndex: '1'}}>
