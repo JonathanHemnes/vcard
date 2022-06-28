c655b8ef32fe1e27a799ef3206fa82c59f08f1e3
Jonathan Hemnes <jhemnes+github@gmail.com>
2022-06-28 12:53:32 -0600

    fix: positioning

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -6,7 +6,7 @@ export function DownloadButton() {
 		window.location = "/vcard/vcard_ely_hemnes.vcf"
 	}
 	return (
-		<Box sx={{position: 'fixed', right: '50px', bottom: '40px', zIndex: '1'}}>
+		<Box sx={{position: 'fixed', right: [ '20px', '40px', '50px'  ], bottom: [  '10px', '25px', '40px' ], zIndex: '1'}}>
 		<Button onClick={handleDownload} >Save Contact Data</Button>
 		</Box>
 	)
